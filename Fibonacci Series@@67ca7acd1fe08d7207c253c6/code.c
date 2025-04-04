2c7d0f40a5df0eae33b9db0f1d65e4a900fbb5a5
MehakSharma14 <mehaksharma200614@gmail.com>
2025-04-04 20:24:33 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void fibo(int n){
+int a= 0, b=1, next;
+for(int i=1; i<=n;i++){
+    printf("%d", a);
+    next = a+b;
+    a=b;
+    b= next;
+}
+}
+int main(){
+    int n;
+    scanf("%d", &n);
+    if(n<=0){
+        printf("Invalid input\n");
+        return 0;
+    }
+    fibo(n);
+    return 0;
+}
